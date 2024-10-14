d6928c799d9c0d6aa640167827935fcd4e3c983a
Aditya Joshi <aditya03joshi@gmail.com>
2024-10-14 12:04:46 +0530

    java

---
+++ src/recursion/programs/searchLinear.java
@@ -0,0 +1,15 @@
+package recursion.programs;
+
+public class searchLinear {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4};
+        int target = 5 ;
+        System.out.println(search(arr,target,0));
+    }
+    static boolean search(int [] arr, int target, int index) {
+        if (index == arr.length) {
+            return false ;
+        }
+     return  arr[index] == target || search(arr,target,index+1) ;
+    }
+}
