f79d3ad6cf0f0f729a25168835963d56ddb1f6d1
Aditya Joshi <aditya03joshi@gmail.com>
2024-10-22 13:30:29 +0530

    java

--- src/recursion/strings/subsequence.java
+++ src/recursion/strings/subsequence.java
@@ -1,8 +1,10 @@
 package recursion.strings;
 
+import java.util.ArrayList;
+
 public class subsequence {
     public static void main(String[] args) {
-        sub("","abcd");
+        System.out.println( subsequenceList("","abcd"));
     }
     static void sub(String p, String up) {
         if (up.isEmpty()) {
@@ -12,5 +14,18 @@ public class subsequence {
         char ch = up.charAt(0);
         sub(p+ch,up.substring(1));
         sub(p ,up.substring(1));
+    }
+    // return an array list
+
+    static ArrayList<String> subsequenceList(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> l = new ArrayList<>();
+            l.add(p);
+            return l;
+        }
+        ArrayList<String> left = subsequenceList(p + up.charAt(0) , up.substring(1));
+        ArrayList<String> right = subsequenceList(p,up.substring(1));
+        left.addAll(right);
+        return left ;
     }
 }
