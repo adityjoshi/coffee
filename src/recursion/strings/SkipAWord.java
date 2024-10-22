c492b4eefd151a82207d35b2929aded56b9c9f20
Aditya Joshi <aditya03joshi@gmail.com>
2024-10-22 13:00:39 +0530

    java

--- src/recursion/strings/SkipAWord.java
+++ src/recursion/strings/SkipAWord.java
@@ -3,6 +3,8 @@ package recursion.strings;
 public class SkipAWord {
     public static void main(String[] args) {
         System.out.println(skipApple("heyappleis"));
+        System.out.println(skipAppNotApple("heyappeis"));
+
     }
     static String skipApple( String up) {
        if (up.isEmpty()) {
@@ -14,5 +16,17 @@ public class SkipAWord {
        else {
            return up.charAt(0) + skipApple(up.substring(1));
        }
+    }
+    // skip something else if the one we need is not present
+    static String skipAppNotApple( String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+        if (up.startsWith("app") && !up.startsWith("apple")) {
+            return skipAppNotApple(up.substring(3));
+        }
+        else {
+            return up.charAt(0) + skipAppNotApple(up.substring(1));
+        }
     }
 }
