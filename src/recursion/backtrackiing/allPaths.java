6e7ea0f6e80ec5b2c7cb2da4df7c13bfd7e27f2f
Aditya Joshi <aditya03joshi@gmail.com>
2024-10-24 12:38:05 +0530

    java

--- src/recursion/backtrackiing/allPaths.java
+++ src/recursion/backtrackiing/allPaths.java
@@ -2,24 +2,34 @@ package recursion.backtrackiing;
 
 public class allPaths {
     public static void main(String[] args) {
-        path(2,2,"");
+        boolean [] [] mat = {
+                {true,true,true},
+                {true,true,true},
+                {true,true,true},
+        };
+        path(0,0,mat,"");
     }
-    static void path(int r, int c, String p) {
-        if (r == 0 && c == 0) {
+    static void path(int r, int c, boolean [][] maze, String p) {
+        if (r == maze.length -1 && c == maze[0].length-1) {
             System.out.println(p);
             return;
         }
+        if(!maze[r][c]) {
+            return;
+        }
+        maze[r][c]= false ;
+        if (r < maze.length-1) {
+            path(r+1,c,maze,p+"R");
+        }
+        if (c < maze[0].length-1) {
+            path(r,c+1,maze,p+"D");
+        }
         if (r>0) {
-            path(r-1,c,p+"R");
+            path(r-1,c,maze,p+"U");
         }
         if (c>0) {
-            path(r,c-1,p+"D");
+            path(r,c-1,maze,"L");
         }
-        if (r<2) {
-            path(r+1,c,p+"U");
-        }
-        if (c<2) {
-            path(r,c+1,"L");
-        }
+        maze[r][c]= true;
     }
 }
