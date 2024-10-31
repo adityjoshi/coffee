7eee25535f038c4de0f5f6ee59daa96da9537dc2
Aditya Joshi <aditya03joshi@gmail.com>
2024-10-31 14:46:05 +0530

    java

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -2,6 +2,12 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("linked list");
+        LL list = new LL();
+        list.insertElement(3);
+        list.insertElement(8);
+        list.insertElement(6);
+        list.insertElement(4);
+
+        list.display();
     }
 }
