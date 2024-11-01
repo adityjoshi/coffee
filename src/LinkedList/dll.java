06ebfcb7ab21984e0b2be42c053b76e18e896544
Aditya Joshi <aditya03joshi@gmail.com>
2024-11-01 13:22:33 +0530

    java

--- src/LinkedList/dll.java
+++ src/LinkedList/dll.java
@@ -14,6 +14,21 @@ public class dll {
         head = node;
 
     }
+    public void insertLast(int val) {
+        Node node = new Node(val);
+        Node last = head ;
+        node.next = null ;
+        if (head == null) {
+            node.prev = null;
+            head = node ;
+            return ;
+        }
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = node ;
+        node.prev = last ;
+    }
     public void display() {
         Node node = head;
          Node last = null ;
