3880440d2d0b78c4ab1591a6ab300ac1cb1e14d7
Aditya Joshi <aditya03joshi@gmail.com>
2024-11-05 21:50:08 +0530

    java

---
+++ src/LinkedList/CLL.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class CLL {
+    private Node head ;
+    private Node tail ;
+
+    public CLL() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insert(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            head = node;
+            tail = node;
+            return;
+        }
+        tail.next = node;
+        node.next = head ;
+        tail = node ;
+
+    }
+
+    private class Node {
+        int val;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+}
