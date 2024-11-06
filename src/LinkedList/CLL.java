8731a1ecbcc871abde03592ac2d7e581329f081c
Aditya Joshi <aditya03joshi@gmail.com>
2024-11-06 11:28:10 +0530

    java

--- src/LinkedList/CLL.java
+++ src/LinkedList/CLL.java
@@ -11,15 +11,25 @@ public class CLL {
 
     public void insert(int val) {
         Node node = new Node(val);
+        // if the ll is empty then make the first node as both head and tail
         if (head == null) {
             head = node;
             tail = node;
             return;
         }
+        /*\
+         when the list is not empty then the tail will point to the new node
+         and new node will point to the head i.e. the previous node for circular link
+         then the tail will move forward to the new node
+         */
         tail.next = node;
         node.next = head ;
         tail = node ;
 
+    }
+    public void display() {
+        Node node = head ;
+        if ()
     }
 
     private class Node {
