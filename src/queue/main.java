9c0c2314ec528cfa6d53f3f860e3712a3afb77b6
Aditya Joshi <aditya03joshi@gmail.com>
2024-11-24 21:44:32 +0530

    java

---
+++ src/queue/main.java
@@ -0,0 +1,27 @@
+package queue;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class main {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        /*
+
+         deletion <- 5 <- 4 <- 3 <- 2 <-1 <- Insertion
+
+         */
+
+        queue.add(5);
+        queue.add(4);
+        queue.add(3);
+        queue.add(2);
+        queue.add(1);
+        System.out.println(queue);
+        queue.remove();
+        queue.remove();
+        System.out.println(queue);
+    }
+}
