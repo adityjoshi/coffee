fd3ae4f080fa82469d5b5944450cfa767518bd02
Aditya Joshi <aditya03joshi@gmail.com>
2024-11-24 20:06:23 +0530

    java

--- src/stack/main.java
+++ src/stack/main.java
@@ -1,8 +1,17 @@
 package stack;
 
+import java.util.Stack;
+
 public class main {
     public static void main(String[] args) {
         // main for stack programs to call
         System.out.println("stack");
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        System.out.println(stack);
     }
 }
