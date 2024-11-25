a3c2e4587b06b42d648fa6dc73e1ca6440404a81
Aditya Joshi <aditya03joshi@gmail.com>
2024-11-25 12:32:10 +0530

    java

---
+++ src/queue/CircularQueue.java
@@ -0,0 +1,26 @@
+package queue;
+
+public class CircularQueue {
+    protected int [] data ;
+    private static int default_size = 10 ;
+   protected int front = 0 ;
+    protected  int end = 0 ;
+    private int size = 0 ;
+
+    public CircularQueue() {
+        this(default_size);
+    }
+    public CircularQueue(int size) {
+        this.data = new int[size];
+    }
+
+
+    public boolean isFull() {
+        // ptr at the last index ;
+        return size == data.length;
+    }
+    public boolean isEmpty() {
+        // ptr at -1 ;
+        return size == 0;
+    }
+}
