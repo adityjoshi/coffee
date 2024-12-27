18cea2ec2b0080036af0c9e71ae1e57234a4d326
Aditya Joshi <aditya03joshi@gmail.com>
2024-12-27 12:37:54 +0530

    ☕️

--- src/trees/AVL.java
+++ src/trees/AVL.java
@@ -1,27 +1,30 @@
 package trees;
 
-public class BinarySearchTree {
+public class AVL{
     public class Node {
         private int value;
-       private int height;
-       private Node left;
-       private Node right;
+        private int height;
+        private Node left;
+        private Node right;
 
-       private Node(int value) {
-           this.value = value ;
-       }
+        private Node(int value) {
+            this.value = value ;
+        }
 
         public int getValue() {
             return value;
         }
     }
     private Node root;
-    public BinarySearchTree() {
+    public AVL() {
 
     }
-    public int height(Node node) {
+    public int height() {
+        return height(root);
+    }
+    private int height(Node node) {
         if (node == null) {
-            return - 1;
+            return -1;
         }
         return node.height;
     }
@@ -43,7 +46,58 @@ public class BinarySearchTree {
             node.right = insert(val,node.right);
         }
         node.height = Math.max(height(node.left),height(node.right)) + 1 ;
+        return rotate(node) ;
+    }
+
+    private Node rotate(Node node) {
+        if(height(node.left)-height(node.right)>1) {
+            // left heavy
+            if(height(node.left.left)-height(node.right) > 1) {
+                return rightRotate(node);
+            }
+            if(height(node.left.left)-height(node.left.right) < 0) {
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+        if (height(node.left) - height(node.right) < -1) {
+            // right heavy
+            if(height(node.right.left) - height(node.right.right) < 0) {
+                // right-right case
+                return leftRotate(node);
+            }
+            if(height(node.right.left) - height(node.right.right) > 0) {
+                // left right case
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
         return node ;
+    }
+    public Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
+
+        c.right = p;
+        p.left = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+
+        return c;
+    }
+
+    public Node leftRotate(Node c) {
+        Node p = c.right;
+        Node t = p.left;
+
+        p.left = c;
+        c.right = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+
+        return p;
     }
 
 
--- src/trees/main.java
+++ src/trees/main.java
@@ -7,11 +7,16 @@ public class main {
        /*
        Binary tree
         */
-            Scanner scanner = new Scanner(System.in);
-            BinaryTree tree = new BinaryTree();
-            tree.populate(scanner);
-            tree.prettyDisplay();
-            tree.preOrder();
+//            Scanner scanner = new Scanner(System.in);
+//            BinaryTree tree = new BinaryTree();
+//            tree.populate(scanner);
+//            tree.prettyDisplay();
+//        System.out.println("tree preOrder");
+//            tree.preOrder();
+//        System.out.println();
+//        tree.inOrder();
+//        System.out.println();
+//        tree.postOrder();
             /*
             Binary search tree
              */
@@ -19,5 +24,14 @@ public class main {
 //        int [] num = {1,2,3,4,5,6};
 //        x.populatedSorted(num);
 //        x.display();
+
+        /*
+        avl tree
+         */
+        AVL tree = new AVL();
+        for (int i = 0; i < 1000; i++) {
+            tree.insert(i);
+        }
+        System.out.println(tree.height());
     }
 }
